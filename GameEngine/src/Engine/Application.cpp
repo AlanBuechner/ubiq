720d276e60984aeb869b25053215312b15246849
AlanBuechner <alanjamsbuechner@gmail.com>
2019-03-11 15:57:24 -0500

    moved OnAttach and OnDetach to the LayerStach

--- GameEngine/src/Engine/Application.cpp
+++ GameEngine/src/Engine/Application.cpp
@@ -30,13 +30,11 @@ namespace Engine {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* layer)
 	{
 		m_LayerStack.PushOverlay(layer);
-		layer->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e)
