62c53c0510a136c7a5dfbfa3356fba26b3d7b5bf
AlanBuechner <alanjamsbuechner@gmail.com>
2019-05-26 07:51:28 -0500

    Shader class

--- GameEngine/src/Engine/Application.cpp
+++ GameEngine/src/Engine/Application.cpp
@@ -51,6 +51,26 @@ namespace Engine {
 
 		unsigned int indeces[3] = { 0, 1, 2};
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indeces), indeces, GL_STATIC_DRAW);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			layout(location = 0) in vec3 a_Position;
+
+			void main(){
+				gl_Position = vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string fragSrc = R"(
+			#version 330 core
+			layout(location = 0) out vec4 color;
+
+			void main(){
+				color = vec4(0.8, 0.2, 0.3, 1.0);
+			}
+		)";
+
+		m_Shader.reset(new Shader(vertexSrc, fragSrc));
 	}
 
 	Application::~Application()
@@ -80,6 +100,7 @@ namespace Engine {
 			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
+			m_Shader->Bind();
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
--- GameEngine/src/Engine/Application.h
+++ GameEngine/src/Engine/Application.h
@@ -5,6 +5,9 @@
 #include "Window.h"
 #include "LayerStack.h"
 
+// temp
+#include "Engine/Renderer/Shader.h"
+
 namespace Engine
 {
 	class Layer;
@@ -45,6 +48,8 @@ namespace Engine
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+
+		std::unique_ptr<Shader> m_Shader;
 
 	private:
 		static Application* s_Instance;
