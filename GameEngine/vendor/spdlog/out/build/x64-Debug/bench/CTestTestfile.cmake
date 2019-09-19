7fc0de2b34eeb5339dcbb69d90f7772feca53049
AlanBuechner <alanjamsbuechner@gmail.com>
2019-09-19 17:19:06 -0500

    new  computer

---
+++ GameEngine/vendor/spdlog/out/build/x64-Debug/bench/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/spdlog/bench
+# Build directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/spdlog/out/build/x64-Debug/bench
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ GameEngine/vendor/spdlog/out/build/x64-Debug/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/spdlog/tests
+# Build directory: C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/spdlog/out/build/x64-Debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(spdlog-utests "C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/spdlog/out/build/x64-Debug/tests/spdlog-utests.exe")
+set_tests_properties(spdlog-utests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/spdlog/tests/CMakeLists.txt;29;add_test;C:/Users/alanj/source/repos/Ubiq/GameEngine/vendor/spdlog/tests/CMakeLists.txt;0;")
