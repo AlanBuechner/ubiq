159156be4230202cfdeb65297df786bc49ebfdf1
AlanBuechner <alanjamsbuechner@gmail.com>
2020-10-24 16:02:43 -0600

    added premake file for USG-Editor

---
+++ USG-Editor/premake5.lua
@@ -0,0 +1,46 @@
+project "USG"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/GameEngine/vendor/spdlog/include",
+		"%{wks.location}/GameEngine/src",
+		"%{wks.location}/GameEngine/vendor",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.entt}"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "DIST"
+		runtime "Release"
+		optimize "on"
