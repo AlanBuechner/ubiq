b45535adf80d97c07dc001a8564288083111f2eb
AlanBuechner <47373257+AlanBuechner@users.noreply.github.com>
2023-02-04 13:32:21 -0700

    generate build props on setup

--- scripts/Setup.bat
+++ scripts/Setup.bat
@@ -1,2 +1,18 @@
 powershell -ExecutionPolicy Bypass -File GetDXC.ps1 ..\GameEngine\vendor\dxc
+
+(
+	echo ^<Project^>
+	echo  ^<PropertyGroup^>
+	echo    ^<LLVMInstallDir^>%cd%\..\vendor\Compiler^</LLVMInstallDir^>
+	echo    ^<LLVMToolsVersion^>16^</LLVMToolsVersion^>
+	echo  ^</PropertyGroup^>
+	echo ^</Project^>
+) > "../Directory.build.props"
+
+rem echo  \<PropertyGroup\>
+rem echo    <LLVMInstallDir>%cd%\vendor\Compiler</LLVMInstallDir>
+rem echo    <LLVMToolsVersion>16</LLVMToolsVersion>
+rem echo  </PropertyGroup>
+rem echo </Project>
+
 GenerateProjects.bat
