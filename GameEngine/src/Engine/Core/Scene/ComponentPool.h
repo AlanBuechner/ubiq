91909b51ba4151d85c896db19a6beb806908a9e9
Alan Buechner <alanjbuechner1@gmail.com>
2024-06-05 18:28:32 -0500

    added component pool allocation paging

--- GameEngine/src/Engine/Core/Scene/ComponentPool.h
+++ GameEngine/src/Engine/Core/Scene/ComponentPool.h
@@ -2,13 +2,30 @@
 #include "Engine/Core/Core.h"
 #include <memory>
 #include <typeinfo>
+#include <bitset>
 
 namespace Engine
 {
 	using ComponentType = uint64;
 
-	template<uint64 TSize>
-	class SizeComponentPool;
+
+	constexpr uint32 PoolSize = 32;
+	class ComponentPoolPage
+	{
+	public:
+		ComponentPoolPage(uint32 componentSize);
+
+		bool HasEntry(uint32 index) { return m_UsedSlots.test(index); }
+		void SetEntry(uint32 index, bool val) { m_UsedSlots.set(index, val); }
+		void* GetComponentMemory(uint32 index) { return (void*)(m_Data + m_ComponentSize * index); }
+
+	private:
+		uint32 m_ComponentSize;
+		byte* m_Data;
+		std::bitset<PoolSize> m_UsedSlots;
+
+		friend class ComponentPool;
+	};
 
 
 	class ComponentPool
@@ -20,10 +37,7 @@ namespace Engine
 			m_ReflectionClass(reflectedClass), 
 			m_TypeID(reflectedClass.GetTypeID()),
 			m_ComponentSize(reflectedClass.GetSize()),
-			m_SceneStatic(reflectedClass.HasFlag("SceneStatic")),
-			m_NumComponents(1),
-			m_Data((byte*)malloc(reflectedClass.GetSize())),
-			m_FreeSlots({ 0 })
+			m_SceneStatic(reflectedClass.HasFlag("SceneStatic"))
 		{}
 		~ComponentPool();
 
@@ -36,8 +50,9 @@ namespace Engine
 		// returns the location in the pool
 		uint32 Allocate(uint64 entity);
 		void Free(uint64 entity);
-		uint32 GetComponentOffset(uint32 i) { return  m_ComponentSize * i; }
-		void* GetComponentRaw(uint32 i) { return (void*)(m_Data + GetComponentOffset(i)); }
+		uint32 GetIndexInPage(uint32 i) { return i % PoolSize; }
+		ComponentPoolPage& GetPageForIndex(uint32 i) { return m_Pages[i/PoolSize]; }
+		void* GetComponentRaw(uint32 i) { return GetPageForIndex(i).GetComponentMemory(GetIndexInPage(i)); }
 		template<typename T> T* GetComponent(uint32 i) { return (T*)GetComponentRaw(i); }
 		void* GetComponentForEntityRaw(uint32 entityID) { return GetComponentRaw(m_EntityComponentMapping[entityID]); }
 		template<typename T> T* GetComponentForEntity(uint32 entityID) { return GetComponent<T>(m_EntityComponentMapping[entityID]); }
@@ -52,7 +67,7 @@ namespace Engine
 		std::vector<uint32> m_FreeSlots;
 		std::vector<uint32> m_UsedSlots;
 
-		uint32 m_NumComponents;
-		byte* m_Data;
+		uint32 m_NumComponents = 0;
+		std::vector<ComponentPoolPage> m_Pages;
 	};
 }
